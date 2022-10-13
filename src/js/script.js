fed4c5d3f6859f4c0bac71ce18d8d69193663daf
nikitaserov <nikiserov1997js@gmail.com>
2022-10-13 21:38:46 +0400

    add function closeMobileMenu

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,19 @@
-const burger = document.querySelector(".burger-menu")
-const navContainer = document.querySelector(".navigation-container")
+const burger = document.querySelector(".burger-menu");
+const navContainer = document.querySelector(".navigation-container");
 
 function toggleMobileMenu() {
     navContainer.classList.toggle("visible");
     burger.classList.toggle("opened");
 }
 
-burger.addEventListener("click", toggleMobileMenu)
+function closeMobileMenu(e) {
+    const element = e.target;
 
+    if (!element.closest(".page-header")) {
+        burger.classList.remove("opened");
+        navContainer.classList.remove("visible");
+    }
+}
+
+document.addEventListener("click", closeMobileMenu);
+burger.addEventListener("click", toggleMobileMenu);
