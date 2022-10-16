5fb57295dd73edc7563a99c7504ddf48b0690a98
nikitaserov <nikiserov1997js@gmail.com>
2022-10-16 16:06:01 +0400

    add modal form

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector(".burger-menu");
 const navContainer = document.querySelector(".navigation-container");
+const openModal = document.querySelector(".checkout-btn");
+const closeModal = document.querySelector(".modal-close");
+const modal = document.querySelector(".modal");
 
 function toggleMobileMenu() {
     navContainer.classList.toggle("visible");
@@ -17,3 +20,12 @@ function closeMobileMenu(e) {
 
 document.addEventListener("click", closeMobileMenu);
 burger.addEventListener("click", toggleMobileMenu);
+
+openModal.addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.classList.add('active');
+})
+
+closeModal.addEventListener("click", function () {
+        modal.classList.remove("active");
+    })
